3f03173649136e5fd4eef7c5fd1a0023391eb87f
Filip <filepix13@gmail.com>
2021-05-07 12:32:40 +0200

    commit_11

--- grafy/src/main.cpp
+++ grafy/src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include "graphs/adjacency_list_graph.hpp"
+#include "graphs/adjacency_matrix_graph.hpp"
+#include "graphs/shortest_path_algorithms.hpp"
+#include <filesystem>
+#include <fstream>
+
+using namespace std::string_literals;
 
 int main(int argc, char* argv[])
 {
-    std::cout<< "Tu wykonujemy testy efektywności algorytmów"<<std::endl;
+    std::ifstream inputStream{"10_100%.txt"};
+    auto graph = AdjacencyMatrixGraph::createGraph(inputStream);
+
+    ShortestPathResult result;
+
+    int sourceIndex;
+    inputStream >> sourceIndex;
+
+    dijkstra(*graph, sourceIndex, result);
+    
+
     return 0;
 }
